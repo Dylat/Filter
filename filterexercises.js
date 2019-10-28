d24d150822df66bca28d2af2ba0661715f581ef2
Dylat <dylanatkins78@gmail.com>
2019-10-28 16:47:56 -0400

    Filter Exercise

---
+++ filterexercises.js
@@ -0,0 +1,22 @@
+//FILTER EXERCISES
+/*Exercise 1
+
+const numbers = [1,2,3,4,5,6,7,8,9,10]
+var outcome= numbers.filter(n=>{return n%2===0});
+console.log(outcome)
+
+Exercise 2  Show only numbers divisible by 5
+const numbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+var outcome= numbers.filter(n=>{return n%5===0});
+console.log(outcome)
+
+Exercise 3 Filter all buzzwords that contains cloud, html, and data
+const h = require('hyperscript')
+const buzzwords = require('buzzwords')
+
+const contains = (w, v) => v.indexOf(w) !== -1
+results = buzzwords.filter(
+  v => contains('cloud', v) || contains('html', v) || contains('data', v)
+)
+console.log(results)
+*/
